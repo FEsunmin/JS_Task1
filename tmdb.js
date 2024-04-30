eeebad0fd992ba94498ab02c0bc428c474282ed7
FE_sunmin <fe.sunmin@gmail.com>
2024-04-30 10:16:54 +0900

    modified: Add SweetAlert2

--- tmdb.js
+++ tmdb.js
@@ -1,43 +1,42 @@
 function tmdbApi() {
-  const apiKey = 'f5475cb87195d22e7fbee353e3247ba5';
+  const apiKey = "f5475cb87195d22e7fbee353e3247ba5";
   const apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
 
   fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => movieData(data.results))
-      .catch(error => console.error('Error fetching movies:', error));
+    .then((response) => response.json())
+    .then((data) => movieData(data.results))
+    .catch((error) => console.error("Error fetching movies:", error));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   tmdbApi();
 });
 
 function movieData(datas) {
-  const cardFlex = document.getElementById('cardFlex');
+  const cardFlex = document.getElementById("cardFlex");
 
-  datas.forEach(movies => {
-      const movieCard = createCard(movies);
-      cardFlex.appendChild(movieCard);
+  datas.forEach((movies) => {
+    const movieCard = createCard(movies);
+    cardFlex.appendChild(movieCard);
 
-      movieCard.addEventListener('click', () => {
-        movieId(movies.id);
-      })
+    movieCard.addEventListener("click", () => {
+      movieId(movies.id);
+    });
   });
 }
 
-  function createCard(movie) {
-    const movieImage = movie.poster_path;
-    const movieTitle = movie.title;
-    const overview = movie.overview;
-    const vote_average = movie.vote_average.toFixed(2);
-  
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('card');
-    movieCard.classList.add('movieCard');
-  
-    movieCard.innerHTML = 
-      `
-        <div class="cardFrame>
+function createCard(movie) {
+  const movieImage = movie.poster_path;
+  const movieTitle = movie.title;
+  const overview = movie.overview;
+  const vote_average = movie.vote_average.toFixed(2);
+
+  const movieCard = document.createElement("div");
+  movieCard.classList.add("card");
+  movieCard.classList.add("movieCard");
+
+  movieCard.innerHTML = `
+        <article class="cardFrame>
           <div class="cardHeader">
             <img src="https://image.tmdb.org/t/p/w300${movieImage}" class="posterImg" alt="poster"/>
           </div>
@@ -49,25 +48,25 @@ function movieData(datas) {
               <p class="movieVote">평점<br>${vote_average} 점</p>
             </div>
           </div>
-        <div>
+        <article>
 
       `;
-    return movieCard;
-  }
+  return movieCard;
+}
 
-    // 영화 아이디 알림창
-    function movieId(id) {
-      alert(`해당 영화의 아이디 값은 : ${id}번 입니다`);
-    }  
+// 영화 아이디 알림창
+function movieId(id) {
+  swal.fire(`해당 영화의 id 값은 : ${id}번 입니다`);
+}
 
-  // 영화 검색
-  const searchInput = document.getElementById('searchBar');
-  searchInput.addEventListener('input', () => {
-    const searchTerm = searchInput.value.toLowerCase();
-    const movieCards = Array.from(document.querySelectorAll('.movieCard'));
-    
-    movieCards.forEach(card => {
-      const title = card.querySelector('.movieTitle').textContent.toLowerCase();
-      card.style.display = title.includes(searchTerm) ? 'block' : 'none';
-    });
+// 영화 검색
+const searchInput = document.getElementById("searchBar");
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  const movieCards = Array.from(document.querySelectorAll(".movieCard"));
+
+  movieCards.forEach((card) => {
+    const title = card.querySelector(".movieTitle").textContent.toLowerCase();
+    card.style.display = title.includes(searchTerm) ? "block" : "none";
   });
+});
